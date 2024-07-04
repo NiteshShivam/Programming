3e42644c2305cb2c7a13e69781f40103cf9c77b0
Nitesh Yadav <42939377+NiteshShivam@users.noreply.github.com>
2024-07-04 15:25:05 +0530

    Update and rename 1509. Minimum Difference Between Largest and Smallest Value in Three Moves to 1509. Minimum Difference Between Largest and Smallest Value in Three Moves.py

--- array/1509. Minimum Difference Between Largest and Smallest Value in Three Moves.py
+++ array/1509. Minimum Difference Between Largest and Smallest Value in Three Moves.py
@@ -1,9 +1,9 @@
-You are given an integer array nums.
+'''You are given an integer array nums.
 
 In one move, you can choose one element of nums and change it to any value.
 
 Return the minimum difference between the largest and smallest value of nums after performing at most three moves.
-
+'''
 Approach 1:
 
 class Solution:
