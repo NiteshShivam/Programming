8de6607c7ccd00786f7b7678ff01b31a999a845c
Nitesh Yadav <42939377+NiteshShivam@users.noreply.github.com>
2024-07-04 15:17:01 +0530

    Update and rename DFS of Graph to DFS of Graph.py

--- Graph/DFS of Graph.py
+++ Graph/DFS of Graph.py
@@ -1,9 +1,9 @@
-You are given a connected undirected graph. Perform a Depth First Traversal of the graph.
+'''You are given a connected undirected graph. Perform a Depth First Traversal of the graph.
 Note: Use the recursive approach to
 find the DFS traversal of the graph starting from the 0th vertex from left to right according to the graph.
 
-
-Approach :
+'''
+# Approach :
 
 class Solution:
     def dfs(self,visited,adj,result,root):
