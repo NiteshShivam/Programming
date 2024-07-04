5966ec6b81e78895270af0bae0ebf65f98ec0ef3
Nitesh Yadav <42939377+NiteshShivam@users.noreply.github.com>
2024-07-04 15:28:42 +0530

    Update and rename Trapping Rain Water to Trapping Rain Water.py

--- array/Trapping Rain Water.py
+++ array/Trapping Rain Water.py
@@ -1,6 +1,6 @@
-Given an array arr[] of N non-negative integers representing the height of blocks.
+'''Given an array arr[] of N non-negative integers representing the height of blocks.
 If width of each block is 1, compute how much water can be trapped between the blocks during the rainy season. 
- 
+ '''
 Approach : 
 
 class Solution:
