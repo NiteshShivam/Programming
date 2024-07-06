99a40dc9bd50ee3903d9864e814bf058a1ba4ab6
Nitesh Yadav <42939377+NiteshShivam@users.noreply.github.com>
2024-07-06 13:11:02 +0530

    Create Binary Tree from Inorder and Postorder.py

---
+++ Tree/Binary Tree from Inorder and Postorder.py
@@ -0,0 +1,45 @@
+#User function Template for python3
+'''
+Given inorder and postorder traversals of a binary tree(having n nodes) in the arrays in[] and post[] respectively. The task is to construct a unique binary tree from these traversals.
+
+Driver code will print the preorder traversal of the constructed tree.
+
+Note :- The inorder and postorder traversals contain unique values, and every value present in the postorder traversal is also found in the inorder traversal.
+'''
+'''
+class Node:
+            def __init__(self, data):
+                self.data = data
+                self.left = self.right = None
+'''
+
+#Function to return a tree created from postorder and inoreder traversals.
+class Solution:
+    def __init__(self):
+        self.In = None
+        self.post = None
+        self.idx=0
+    def solve(self,start,end):
+        if start>end:
+            return None
+        root = Node(self.post[self.idx])
+        rootVal = self.post[self.idx]
+        i = start
+        while i<=end:
+            if self.In[i]==rootVal:
+                break
+            i+=1
+        self.idx = self.idx-1
+        root.right = self.solve(i+1,end)
+        root.left = self.solve(start,i-1)
+        
+        return root
+            
+    def buildTree(self,n , In, post):
+        start = 0
+        end = n-1
+        self.idx = n-1
+        self.In = In
+        self.post = post
+        return self.solve(start,end)
+    
