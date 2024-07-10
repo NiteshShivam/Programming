552c35dbf9450a579e9c758590c681606e25cc5f
Nitesh Yadav <42939377+NiteshShivam@users.noreply.github.com>
2024-07-10 16:33:49 +0530

    Update Longest Increasing Subsequence.py

--- array/Longest Increasing Subsequence.py
+++ array/Longest Increasing Subsequence.py
@@ -1,4 +1,14 @@
+'''
+Given an array a[ ] of n integers, find the Length of the Longest Strictly Increasing Subsequence.
 
+A sequence of numbers is called "strictly increasing" when each
+term in the sequence is smaller than the term that comes after it.
+
+
+https://www.geeksforgeeks.org/problems/longest-increasing-subsequence-1587115620/1
+https://www.youtube.com/watch?v=DG50PJIx2SM
+https://www.youtube.com/watch?v=h9rm4N8XbL0
+'''
 class Solution:
     
     #Function to find length of longest increasing subsequence.
@@ -13,3 +23,26 @@ class Solution:
         for i in range(n):
             dp[0] = max(dp[0],dp[i])
         return dp[0]
+===========================
+Approach 2:
+
+class Solution:
+
+    #Function to find length of longest increasing subsequence.
+    def longestSubsequence(self, n, arr):
+        # code here
+        dp = [[-1]*(n+1) for i in range(n+1)]
+        def solve(i,p):
+            if i>=n:
+                return 0
+            take,skip=0,0
+            if dp[i][p]!=-1:
+                return dp[i][p]
+            if p==-1 or arr[p]<arr[i]:
+                take = 1+solve(i+1,i)
+            skip = solve(i+1,p)
+            if p!=-1:
+                dp[i][p] = max(take,skip)
+            return max(take,skip)
+            
+        return solve(0,-1)
