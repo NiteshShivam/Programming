4091f8c515048ea1de3b462e4da05c66986bd7f0
Nitesh Yadav <42939377+NiteshShivam@users.noreply.github.com>
2024-07-11 15:16:39 +0530

    Update 1190. Reverse Substrings Between Each Pair of Parentheses.py

--- String/1190. Reverse Substrings Between Each Pair of Parentheses.py
+++ String/1190. Reverse Substrings Between Each Pair of Parentheses.py
@@ -1,4 +1,6 @@
 '''
+
+https://www.youtube.com/watch?v=dUQRS4luBvA&t=959s
 https://leetcode.com/problems/reverse-substrings-between-each-pair-of-parentheses/description/
 '''
 from collections import deque
@@ -20,3 +22,20 @@ class Solution:
             if space[i]!="(" and space[i]!=")":
                 result+=space[i]
         return result
+==================================================================
+
+from collections import deque
+class Solution:
+    def reverseParentheses(self, s: str) -> str:
+        stack = deque()
+        length = len(s)
+        send  =""
+        for i in range(length):
+            if s[i]=="(":
+                stack.append(len(send))
+            elif s[i]==")":
+                t = stack.pop()
+                send = send[:t]+send[t:][::-1]   
+            else:
+                send+=s[i]
+        return send
