a65c8b9f00f34f7e5d4b9976b81c2b8fde1a53c0
Nitesh Yadav <42939377+NiteshShivam@users.noreply.github.com>
2024-07-11 10:10:34 +0530

    Update Maximum Connected group.py

--- Graph/Maximum Connected group.py
+++ Graph/Maximum Connected group.py
@@ -1,3 +1,8 @@
+'''
+https://www.geeksforgeeks.org/problems/maximum-connected-group/1
+
+only Approach 3 passing the test case
+'''
 class Solution:
     def __init__(self):
         self.arr = None
@@ -98,3 +103,59 @@ class Solution:
                                 seen.add(root)
                     max_group_size=max(max_group_size,new_size)
         return max_group_size
+
+===========================
+Approach 3:
+
+from typing import List
+from collections import deque
+
+class Solution:
+    def MaxConnection(self, grid : List[List[int]]) -> int:
+        n = len(grid)
+        def dfs(x,y,visited):
+            stack = deque()
+            stack.append((x,y))
+            visited.add((x,y))
+            size = 0
+            component = [(x,y)]
+            while stack:
+                cx,cy = stack.pop()
+                size+=1
+                for dx,dy in [(-1,0),(0,-1),(1,0),(0,1)]:
+                    nx,ny = dx+cx,cy+dy
+                    if 0<=nx<n and 0<=ny<n and grid[nx][ny] and (nx,ny) not in visited:
+                        visited.add((nx,ny))
+                        stack.append((nx,ny))
+                        component.append((nx,ny))
+            return size,component
+        visited =set()
+        group_index = 0
+        group_sizes= {}
+        index_grid = [[-1]*n for _ in range(n)]
+        for i in range(n):
+            for j in range(n):
+                if grid[i][j] and (i,j) not in visited:
+                    size,component = dfs(i,j,visited)
+                    group_sizes[group_index] = size
+                    for x,y in component:
+                        index_grid[x][y]=group_index
+                    group_index+=1
+        max_size = max(group_sizes.values(),default=0)
+        
+        
+        for i in range(n):
+            for j in range(n):
+                if grid[i][j]==0:
+                    seen = set()
+                    new_size = 1
+                    for dx,dy in [(-1,0),(1,0),(0,-1),(0,1)]:
+                        ni,nj = i+dx,j+dy
+                        if 0<=ni<n and 0<=nj < n and grid[ni][nj]:
+                            group_id = index_grid[ni][nj]
+                            if group_id not in seen:
+                                new_size+=group_sizes[group_id]
+                                seen.add(group_id)
+                    max_size = max(max_size,new_size)
+        return max_size
+        
