494f54cab0e2c13b01ac480aabb74e0f62523ddd
Nitesh Yadav <42939377+NiteshShivam@users.noreply.github.com>
2024-07-11 09:22:39 +0530

    Update Maximum Connected group.py

--- Graph/Maximum Connected group.py
+++ Graph/Maximum Connected group.py
@@ -28,3 +28,73 @@ class Solution:
                 self.result = max(self.result,self.dfs(i,j,visited))
         return self.result
                 
+=============================
+
+from typing import List
+class UnionFind:
+    def __init__(self,size):
+        self.parent = list(range(size))
+        self.rank = [1]*size
+        self.size = [1]*size
+    def find(self,x):
+        if self.parent[x]!=x:
+            self.parent[x]=self.find(self.parent[x])
+        return self.parent[x]
+    def union(self,x,y):
+        rootX = self.find(x)
+        rootY = self.find(y)
+        if rootX!=rootY:
+            if self.rank[rootX]>self.rank[rootY]:
+                self.parent[rootY]=rootX
+                self.size[rootX] +=self.size[rootY]
+            elif self.rank[rootX]<self.rank[rootY]:
+                self.parent[rootX]=rootY
+                self.size[rootY]+=self.size[rootX]
+            else:
+                self.parent[rootY]=rootX
+                self.rank[rootX]=1+self.rank[rootX]
+                self.size[rootX]+=self.size[rootY]
+            
+    def get_size(self,x):
+        root = self.find(x)
+        return self.size[root]
+class Solution:
+    def __init__(self):
+        self.directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+
+    def MaxConnection(self, grid: List[List[int]]) -> int:
+        n = len(grid)
+        uf = UnionFind(n*n)
+        def index(x,y):
+            return x*n+y
+        for i in range(n):
+            for j in range(n):
+                if grid[i][j]==1:
+                    for direction in self.directions:
+                        ni,nj = i+direction[0],j+direction[1]
+                        if 0<=ni<n and 0<=nj<n and grid[ni][nj]==1:
+                            uf.union(index(i,j),index(ni,nj))
+        max_group_size=0
+        component_size_cache = {}
+        for i in range(n):
+            for j in range(n):
+                if grid[i][j]==1:
+                    root = uf.find(index(i, j))
+                    if root not in component_size_cache:
+                        component_size_cache[root] = uf.get_size(index(i, j))  # Cache the component size
+                    max_group_size = max(max_group_size, component_size_cache[root])
+        
+        for i in range(n):
+            for j in range(n):
+                if grid[i][j]==0:
+                    seen = set()
+                    new_size=1
+                    for direction in self.directions:
+                        ni,nj = i+direction[0],j+direction[1]
+                        if 0<=ni<n and 0<=nj<n and grid[ni][nj]==1:
+                            root = uf.find(index(ni,nj))
+                            if root not in seen:
+                                new_size += component_size_cache[root]
+                                seen.add(root)
+                    max_group_size=max(max_group_size,new_size)
+        return max_group_size
