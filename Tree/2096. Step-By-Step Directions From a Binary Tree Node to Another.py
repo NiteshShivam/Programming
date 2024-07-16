ea9b5ada3bb5bf61ebdfb2d16bca1e5800ef6608
Nitesh Yadav <42939377+NiteshShivam@users.noreply.github.com>
2024-07-16 09:42:10 +0530

    Update 2096. Step-By-Step Directions From a Binary Tree Node to Another.py

--- Tree/2096. Step-By-Step Directions From a Binary Tree Node to Another.py
+++ Tree/2096. Step-By-Step Directions From a Binary Tree Node to Another.py
@@ -30,3 +30,48 @@ class Solution:
         t =""
         t+="U"*len(sPath)
         return t + ''.join(reversed(dPath))
+
+
+
+Approach 2:
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+from collections import defaultdict,deque
+class Solution:
+    def getDirections(self, root: Optional[TreeNode], startValue: int, destValue: int) -> str:
+        adj = defaultdict(list)
+        def inorder(root):
+            if not root:
+                return
+            if root.left:
+                adj[root.val].append((root.left.val,"L"))
+                adj[root.left.val].append((root.val,"U"))
+                inorder(root.left)
+            if root.right:
+                adj[root.val].append((root.right.val,"R"))
+                adj[root.right.val].append((root.val,"U"))
+                inorder(root.right)
+        inorder(root)
+        def bfs(startValue,destValue):
+            que = deque()
+            visited = set()
+            que.append((startValue,""))
+            visited.add(startValue)
+            while que:
+                nodeV,path = que.popleft()
+                if nodeV==destValue:
+                    return path
+                for child,st in adj[nodeV]:
+                    if child not in visited:
+                        visited.add(child)
+                        que.append((child,path+st))
+            return ""
+        
+
+                        
+                        
+        return bfs(startValue,destValue)
