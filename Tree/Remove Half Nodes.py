c9254b6d278412f96851d149c7bc0328ead44c6d
Nitesh Yadav <42939377+NiteshShivam@users.noreply.github.com>
2024-07-20 05:17:53 +0530

    Update Remove Half Nodes.py

--- Tree/Remove Half Nodes.py
+++ Tree/Remove Half Nodes.py
@@ -21,3 +21,20 @@ class Solution:
     def RemoveHalfNodes(self, node):
         root = self.dfs(node)
         return root
+
+
+=========================================
+class Solution:
+    def RemoveHalfNodes(self, root):
+        if not root:
+            return None
+        if not root.left and not root.right:
+            return root
+        root.left = self.RemoveHalfNodes(root.left)
+        root.right = self.RemoveHalfNodes(root.right)
+        if root.left and not root.right:
+            return root.left
+        if not root.left and root.right:
+            return root.right
+        return root
+
