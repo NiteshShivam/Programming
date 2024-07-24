fd13926f7e11228afd4bb6ff634fd7ef8187216c
Nitesh Yadav <42939377+NiteshShivam@users.noreply.github.com>
2024-07-24 10:02:18 +0530

    Create Row with max 1s.py

---
+++ array/Row with max 1s.py
@@ -0,0 +1,46 @@
+'''
+
+https://www.geeksforgeeks.org/problems/row-with-max-1s0023/1
+'''
+class Solution:
+
+	def rowWithMax1s(self,arr):
+	    row = len(arr)
+	    col = len(arr[0])
+	    prevjS = col+1
+	    prevR = -1
+	    for i in range(row):
+	        left = 0
+	        right = col-1
+	        prevj = -1
+	        while left<=right:
+	            mid = left+(right-left)//2
+	            if arr[i][mid]==0:
+	                left = mid+1
+	            elif arr[i][mid]==1:
+	                prevj=mid
+	                right = mid-1
+	           
+	        if prevj !=-1:
+	            if prevjS>prevj:
+	                prevjS=prevj
+	                prevR = i
+	        
+	    return prevR
+
+
+
+
+
+=====================================
+#User function Template for python3
+class Solution:
+
+    def rowWithMax1s(self,arr):
+        col = len(arr[0])
+        row = len(arr)
+        for i in range(col):
+            for j in range(row):
+                if arr[j][i]==1:
+                    return j
+        return -1
