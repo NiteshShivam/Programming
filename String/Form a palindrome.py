77c36a986da6bcad6ecc42c043ac9a6c327cc2bd
Nitesh Yadav <42939377+NiteshShivam@users.noreply.github.com>
2024-07-27 06:51:48 +0530

    Update Form a palindrome.py

--- String/Form a palindrome.py
+++ String/Form a palindrome.py
@@ -2,6 +2,8 @@
 
 https://www.geeksforgeeks.org/problems/form-a-palindrome1455/1
 https://www.youtube.com/watch?v=99S-xuAbwoc
+
+https://www.youtube.com/watch?v=jEDPdAwnxV0
 '''
 class Solution:
     def solve(self,i,j,s):
@@ -21,3 +23,26 @@ class Solution:
         self.dp =[[-1]*j for _ in range(j)]
         j = j-1
         return self.solve(0,j,str)
+
+
+
+
+
+
+=========================================
+
+
+class Solution:
+    def countMin(self, str):
+        length = len(str)
+        dp = [[0]*length for i in range(length)]
+        
+        for L in range(2,length+1):
+            for i in range(length-L+1):
+                j = i+L-1
+                if str[i] ==str[j]:
+                    dp[i][j]=dp[i+1][j-1]
+                else:
+                    dp[i][j] = 1 + min(dp[i+1][j],dp[i][j-1])
+            
+        return dp[0][length-1]
