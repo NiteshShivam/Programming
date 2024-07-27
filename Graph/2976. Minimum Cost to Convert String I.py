07400b8887929cff936399c9a26df4ebfb4ec6f0
Nitesh Yadav <42939377+NiteshShivam@users.noreply.github.com>
2024-07-27 14:54:34 +0530

    Update 2976. Minimum Cost to Convert String I.py

--- Graph/2976. Minimum Cost to Convert String I.py
+++ Graph/2976. Minimum Cost to Convert String I.py
@@ -30,3 +30,49 @@ class Solution:
                 result = result+matrix[s][d]
 
         return result        
+
+
+
+
+
+=====================================
+import heapq
+class Solution:
+    def dijkstra(self,source,adj,distance):
+        distance[source]=0
+        heap = []
+        heapq.heappush(heap,(0,source))
+        while heap:
+            temp = heapq.heappop(heap)
+            cost = temp[0]
+            node = temp[1]
+            for each in adj[node]:
+                w = each[0]
+                v = each[1]
+                if distance[node]+w<distance[v]:
+                    distance[v] = distance[node]+w
+                    heapq.heappush(heap,(distance[node]+w,v))
+
+    def minimumCost(self, source: str, target: str, original: List[str], changed: List[str], cost: List[int]) -> int:
+        length = len(original)
+        adj = [[] for _ in range(26)]
+        for i in range(length):
+            s = ord(original[i])-ord('a')
+            d = ord(changed[i])-ord('a')
+            adj[s].append((cost[i],d))
+        maxV = float('inf')
+        matrix = [[maxV]*26 for i in range(26)]
+        
+        for i in range(len(source)):
+            s = ord(source[i])-ord('a')
+            self.dijkstra(s,adj,matrix[s])
+        result = 0
+        for i in range(len(source)):
+            if source[i]!=target[i]:
+                s = ord(source[i])-ord('a')
+                d = ord(target[i])-ord('a')
+                if matrix[s][d]==maxV:
+                    return -1
+                result = result+matrix[s][d]
+
+        return result        
