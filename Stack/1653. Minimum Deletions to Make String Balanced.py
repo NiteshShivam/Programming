14171a54d76d8ed82bc79a798d3c16fd82f42de7
Nitesh Yadav <42939377+NiteshShivam@users.noreply.github.com>
2024-07-30 10:50:19 +0530

    Update 1653. Minimum Deletions to Make String Balanced.py

--- Stack/1653. Minimum Deletions to Make String Balanced.py
+++ Stack/1653. Minimum Deletions to Make String Balanced.py
@@ -17,3 +17,29 @@ class Solution:
             else:
                 stack.append(ch)
         return result
+
+
+===============================
+
+class Solution:
+    def minimumDeletions(self, s: str) -> int:
+        result = 0
+        a = []
+        length = len(s)
+        b = [0]*length
+        bcount = 0
+        acount = 0
+        i = -1
+        for ch in s:
+            a.append(acount)
+            if ch=='b':
+                acount+=1
+            b[i]=bcount
+            if s[i]=='a':
+                bcount+=1
+            i-=1
+        result = 2*length
+        for i in range(length):
+            result = min(result,a[i]+b[i])
+        return result
+        return result
