fc3b0795a36b1c333ee2113e01ab655f2662789d
Nitesh Yadav <42939377+NiteshShivam@users.noreply.github.com>
2024-07-31 16:12:18 +0530

    Update Edit Distance.py

--- Dynamic Programming/Edit Distance.py
+++ Dynamic Programming/Edit Distance.py
@@ -3,6 +3,36 @@
 https://youtu.be/oqfqPEqrKo8
 https://www.geeksforgeeks.org/problems/edit-distance3702/1
 '''
+class Solution:
+    def solve(self,i,j):
+        if i==self.m or j==self.n:
+            if j==self.n:
+                return self.m-i
+            return self.n-j
+        else:
+            if self.dp[i][j]!=-1:
+                return self.dp[i][j]
+            if self.s[i]==self.t[j]:
+                return self.solve(i+1,j+1)
+            # insert
+            insert =1+self.solve(i,j+1)
+            # delete
+            delete = 1+self.solve(i+1,j)
+            #replace
+            replace =1+ self.solve(i+1,j+1)
+            self.dp[i][j] = min(insert,delete,replace)
+            return min(insert,delete,replace)
+        
+	def editDistance(self, s, t):
+		
+		self.s =s 
+		self.t =t
+		self.m = len(s)
+		self.n = len(t)
+		self.dp = [[-1]*(self.n+1) for _ in range(self.m+1)]
+		return self.solve(0,0)
+
+===========================================
 
 class Solution:
 	def editDistance(self, s, t):
