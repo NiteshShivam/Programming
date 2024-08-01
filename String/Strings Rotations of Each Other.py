c1402fb53b36ea2c1117b5ec4dc534eaf273e1bb
Nitesh Yadav <42939377+NiteshShivam@users.noreply.github.com>
2024-08-01 15:20:05 +0530

    Update Strings Rotations of Each Other.py

--- String/Strings Rotations of Each Other.py
+++ String/Strings Rotations of Each Other.py
@@ -11,3 +11,25 @@ class Solution:
         if s2 in 2*s1:
             return True
         return False
+
+
+
+===========================
+
+class Solution:
+    
+    #Function to check if two strings are rotations of each other or not.
+    def areRotations(self,s1,s2):
+        if len(s1)!=len(s2):
+            return False
+       
+        temp = s1+s1
+        j=0
+        lengths2 = len(s2)
+        for i in range(len(temp)):
+            if j<lengths2 and s2[j]==temp[i]:
+                j+=1
+        if j ==lengths2:
+            return True
+        return False
+        
