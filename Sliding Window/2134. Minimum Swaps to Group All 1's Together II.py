eb02d88ef3fb3fff79f243e8ce0ddbbc2fff2a3c
Nitesh Yadav <42939377+NiteshShivam@users.noreply.github.com>
2024-08-02 06:15:13 +0530

    Create 2134. Minimum Swaps to Group All 1's Together II.py

---
+++ Sliding Window/2134. Minimum Swaps to Group All 1's Together II.py
@@ -0,0 +1,28 @@
+'''
+https://leetcode.com/problems/minimum-swaps-to-group-all-1s-together-ii/description/
+'''
+class Solution:
+    def minSwaps(self, nums: List[int]) -> int:
+        ones = nums.count(1)
+        nums = nums+nums[:ones]
+        i = 0
+        j = ones
+        length = len(nums)
+        current = 0
+        count = 0
+        for k in range(ones):
+            if nums[k]==0:
+                count+=1
+        result = count
+        while j<length:
+            zeroi=0
+            zeroj=0
+            if nums[i]==0:
+                zeroi=1
+            if nums[j]==0:
+                zeroj=1
+            count =count-zeroi+zeroj
+            result = min(result,count)
+            j+=1
+            i+=1
+        return result
