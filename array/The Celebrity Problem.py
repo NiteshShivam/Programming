a530024534055e6cb3f8fe350af12801ed761b66
Nitesh Yadav <42939377+NiteshShivam@users.noreply.github.com>
2024-08-03 05:25:56 +0530

    Update The Celebrity Problem.py

--- array/The Celebrity Problem.py
+++ array/The Celebrity Problem.py
@@ -1,3 +1,7 @@
+'''
+https://www.geeksforgeeks.org/problems/the-celebrity-problem/1
+'''
+
 '''A celebrity is a person who is known to all but does not know anyone at a party. If you go to a party of N people,
 find if there is a celebrity in the party or not.
 A square NxN matrix M[][] is used to represent people at the party such that if an element of row i and column j 
