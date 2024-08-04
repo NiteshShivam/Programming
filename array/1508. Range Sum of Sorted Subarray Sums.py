27acb76db8c8619d99d16c6c538f8ac4fc2deac6
Nitesh Yadav <42939377+NiteshShivam@users.noreply.github.com>
2024-08-04 17:20:45 +0530

    Update 1508. Range Sum of Sorted Subarray Sums.py

--- array/1508. Range Sum of Sorted Subarray Sums.py
+++ array/1508. Range Sum of Sorted Subarray Sums.py
@@ -1,6 +1,29 @@
 '''
 https://leetcode.com/problems/range-sum-of-sorted-subarray-sums/description/
+https://youtu.be/oMHO5iti5_c
 '''
+
+import heapq
+class Solution:
+    def rangeSum(self, nums: List[int], n: int, left: int, right: int) -> int:
+        hp = []
+        mod = 10**9+7
+        for i in range(n):
+            heapq.heappush(hp,(nums[i],i))
+        result = 0
+        count =1
+        while count<=right:
+            value,index = heapq.heappop(hp)
+            if index<(n-1):
+                heapq.heappush(hp,(value+nums[index+1],index+1))
+            if count>=left:
+                result+=value
+            count+=1
+        return result%mod
+
+
+=======================
+
 import heapq
 class Solution:
     def rangeSum(self, nums: List[int], n: int, left: int, right: int) -> int:
