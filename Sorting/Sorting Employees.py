5b81ff5fe015a5b1ebc8533acc80bd0c133d9e13
Nitesh Yadav <42939377+NiteshShivam@users.noreply.github.com>
2024-08-04 11:12:17 +0530

    Update Sorting Employees.py

--- Sorting/Sorting Employees.py
+++ Sorting/Sorting Employees.py
@@ -4,3 +4,24 @@ https://www.geeksforgeeks.org/problems/sorting-employees5907/1
 class Solution:
     def sortRecords(self, a, n):
         a.sort(key=lambda x:(x[1],x[0]))
+
+
+======================
+import functools
+
+class Solution:
+    
+    def sortRecords(self, a, n):
+        def compare(x,y):
+            if x[1]<y[1]:
+                return -1
+            elif x[1]>y[1]:
+                return 1
+            else:
+                if x[0]<y[0]:
+                    return -1
+                elif x[0]>y[0]:
+                    return 1
+                else:
+                    return 0
+        a.sort(key=functools.cmp_to_key(compare))
