039dc7a0055132349e999ff074212dccf9025700
Nitesh Yadav <42939377+NiteshShivam@users.noreply.github.com>
2024-08-04 11:24:26 +0530

    Update Sorting Employees.py

--- Sorting/Sorting Employees.py
+++ Sorting/Sorting Employees.py
@@ -23,5 +23,21 @@ class Solution:
                 elif x[0]>y[0]:
                     return 1
                 else:
+                    
                     return 0
         a.sort(key=functools.cmp_to_key(compare))
+
+
+# Time limit exceed below one.
+=======================
+
+import functools
+
+class Solution:
+    
+    def sortRecords(self, a, n):
+        for i in range(n):
+            for i in range(n-i-1):
+                if a[i][1]>a[i+1][1] or ((a[i][1]==a[i+1][1]) and (a[i][0]>a[i+1][0]) ):
+                    a[i],a[i+1] =a[i+1],a[i]
+        
