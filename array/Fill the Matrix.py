02ad83a8a8b17e5623338ca2abcbc779979fb3c2
Nitesh Yadav <42939377+NiteshShivam@users.noreply.github.com>
2024-08-05 18:11:02 +0530

    Create Fill the Matrix.py

---
+++ array/Fill the Matrix.py
@@ -0,0 +1,29 @@
+'''
+https://www.geeksforgeeks.org/problems/fill-the-matrix--170647/1
+'''
+from collections import deque
+class Solution:
+    def minIteration(self, N, M, x, y):
+        queue = deque()
+        queue.append((x-1,y-1))
+        matrix = [[0]*M for i in range(N)]
+        matrix[x-1][y-1]=1
+        direction = [(-1,0),(0,-1),(1,0),(0,1)]
+        count=0
+        while queue:
+            length = len(queue)
+            flag=0
+            while length>0:
+                q = queue.popleft()
+                i,j = q
+                for each in direction:
+                    i1 = each[0]+i
+                    j1 = each[1]+j
+                    if i1>=0 and i1<N and j1>=0 and j1<M and matrix[i1][j1]==0:
+                        queue.append((i1,j1))
+                        matrix[i1][j1]=1
+                        flag=1
+                length-=1
+            if flag:
+                count+=1
+        return count
