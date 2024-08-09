0a2c004e00af1e6188a615ba99c57c4e6df99a18
Nitesh Yadav <42939377+NiteshShivam@users.noreply.github.com>
2024-08-09 16:43:57 +0530

    Update 840. Magic Squares In Grid.py

--- array/840. Magic Squares In Grid.py
+++ array/840. Magic Squares In Grid.py
@@ -1,5 +1,7 @@
 '''
 https://leetcode.com/problems/magic-squares-in-grid/description/
+
+https://youtu.be/KgiW_AtaBPo
 '''
 class Solution:
     def numMagicSquaresInside(self, grid: List[List[int]]) -> int:
@@ -53,6 +55,70 @@ class Solution:
                 d2+=grid[i+r1][j+c1]
                 r1+=1
                 c1-=1
+            if d2!=row1:
+                return False
+
+            return True
+
+        for i in range(row-2):
+            for j in range(col-2):
+                if isGrid(i,j):
+                    result+=1
+
+        return result
+
+
+
+==================================================
+class Solution:
+    def numMagicSquaresInside(self, grid: List[List[int]]) -> int:
+        result = 0
+        row = len(grid)
+        col = len(grid[0])
+        def isGrid(i,j):
+            row1,row2,row3,col1,col2,col3,d1,d2=0,0,0,0,0,0,0,0
+            unique = set()
+            if grid[i+1][j+1]!=5:
+                return False
+            for k in range(3):
+                for t in range(3):
+                    if grid[i+k][t+j] in unique or grid[i+k][t+j]>9 or grid[i+k][t+j]<=0:
+                        return False
+                    else:
+                        unique.add(grid[i+k][t+j])
+            
+            # for diagonal2
+            r1=0
+            c1 = 2
+
+            for k in range(3):
+                row1 += grid[i][k+j]
+                row2 += grid[i+1][k+j]
+                row3 += grid[i+2][k+j]
+                col1 += grid[i+k][j]
+                col2 += grid[i+k][j+1]
+                col3 += grid[i+k][j+2]
+                d1+= grid[i+k][j+k]
+
+                d2+=grid[i+r1][j+c1]
+                r1+=1
+                c1-=1
+            
+                
+            if row1!=row2:
+                return False
+            if row3!=row1:
+                return False
+            # col
+            if row1!=col1:
+                return False
+            if row1!=col2:
+                return False
+            if row1!=col3:
+                return False
+            # diagonal
+            if d1!=row1:
+                return False
             if d2!=row1:
                 return False
 
