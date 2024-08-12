c9d97ac93f22f07b3accef54d2fba24e1ada4ae5
Nitesh Yadav <42939377+NiteshShivam@users.noreply.github.com>
2024-08-12 07:57:33 +0530

    Update Median of two sorted arrays.py

--- array/Median of two sorted arrays.py
+++ array/Median of two sorted arrays.py
@@ -29,3 +29,58 @@ class Solution:
             t = (length1+length2)//2
             result = temp[t]
         return result
+
+
+
+=========================
+#User function Template for python3
+
+class Solution:
+    def sum_of_middle_elements(self, arr1, arr2):
+        length1 = len(arr1)
+        length2 = len(arr2)
+        t1  = (length1+length2)//2
+        t2 = t1-1
+        ans1= 0
+        ans2=0
+        i=0
+        j=0
+        k = 0
+        while i<length1 and j<length2:
+            if arr1[i]<arr2[j]:
+                if k==t1:
+                    ans1=arr1[i]
+                elif k==t2:
+                    ans2 = arr1[i]
+                k+=1
+                i+=1
+            else:
+                if k==t1:
+                    ans1=arr2[j]
+                elif k==t2:
+                    ans2 = arr2[j]
+                k+=1
+                j+=1
+                
+                
+        while i<length1:
+            if k==t1:
+                ans1=arr1[i]
+            elif k==t2:
+                ans2 = arr1[i]
+            k+=1
+            i+=1
+        while j<length2:
+            if k==t1:
+                ans1=arr2[j]
+            elif k==t2:
+                ans2 = arr2[j]
+            k+=1
+            j+=1
+        result = 0
+        if (length1+length2)%2==0:
+            result = ans1+ans2
+        else:
+           
+            result = ans1
+        return result
