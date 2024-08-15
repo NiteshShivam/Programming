cbbfd16f99d88e73e2827001143d0aab4f3b5f29
Nitesh Yadav <42939377+NiteshShivam@users.noreply.github.com>
2024-08-15 09:42:45 +0530

    Update Add 1 to a Linked List Number.py

--- Linked List/Add 1 to a Linked List Number.py
+++ Linked List/Add 1 to a Linked List Number.py
@@ -30,4 +30,60 @@ class Solution:
             
         head = self.reverse(head)
         return head
+
+
+
+
+
+
+# cpp
+====================
+/*
+
+struct Node
+{
+    int data;
+    struct Node* next;
+
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+
+*/
+
+class Solution {
+  public:
+    Node* reverse(Node *head){
+        Node* temp = head;
+        Node* prev = NULL;
+        Node* temp2 = NULL;
+        while(temp){
+            
+            temp2 = temp->next;
+            temp->next= prev;
+            prev = temp;
+            temp = temp2;
+        }
+        return prev;
+    }
+    Node* addOne(Node* head) {
+        head = reverse(head);
+        Node* temp = head;
+        int current = 1;
+        Node* prev = NULL;
+        while(temp){
+            current = current+temp->data;
+            temp->data = current%10;
+            current=current/10;
+            prev = temp;
+            temp=temp->next;
+        }
+        if(current){
+            prev->next = new Node(current);
+        }
+        return reverse(head);
+    }
+};
         
