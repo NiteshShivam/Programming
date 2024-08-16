af63699558187c20c3167ecdeaf81d9211e71a0f
Nitesh Yadav <42939377+NiteshShivam@users.noreply.github.com>
2024-08-16 05:51:25 +0530

    Update Maximize The Cut Segments.py

--- Dynamic Programming/Maximize The Cut Segments.py
+++ Dynamic Programming/Maximize The Cut Segments.py
@@ -22,3 +22,33 @@ class Solution:
             dp[i] = max(count1,count2,count3)
         # print(dp)
         return dp[n]
+
+# cpp
+=================================
+class Solution
+{
+    public:
+    //Function to find the maximum number of cuts.
+    int maximizeTheCuts(int n, int x, int y, int z)
+    {
+        vector<int> dp(n+1,0);
+        for(int i=1;i<=n;i++){
+            if(i-x>=0 and( (i-x==0) or  dp[i-x]!=0))
+            {
+                dp[i] = max(dp[i],1+dp[i-x]);
+            }
+            if(i-y>=0 and( (i-y==0) or  dp[i-y]!=0))
+            {
+                dp[i] = max(dp[i],1+dp[i-y]);
+            }
+            
+            if(i-z>=0 and( (i-z==0) or  dp[i-z]!=0))
+            {
+                dp[i] = max(dp[i],1+dp[i-z]);
+            }
+            
+            
+        }
+        return dp[n];
+    }
+};
