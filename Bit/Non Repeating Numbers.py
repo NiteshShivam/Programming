93dfde5f2d97a8c6b907e642a3ca2bfb7836e83a
Nitesh Yadav <42939377+NiteshShivam@users.noreply.github.com>
2024-08-17 10:56:49 +0530

    Update Non Repeating Numbers.py

--- Bit/Non Repeating Numbers.py
+++ Bit/Non Repeating Numbers.py
@@ -19,3 +19,34 @@ class Solution:
 		    return [y,x]
 		return [x,y]
 
+
+
+# cpp
+=============================
+class Solution
+{
+public:
+    vector<int> singleNumber(vector<int> nums) 
+    {
+        int result=0;
+        int length = nums.size();
+        for(int i=0;i<length;i++){
+            result = result^nums[i];
+        }
+        int number = result & -result;
+        int x = 0;
+        int y=0;
+        for(int i=0;i<length;i++){
+            if(nums[i]&number){
+                x = x^nums[i];
+            }
+            else{
+                y = y^nums[i];
+            }
+        }
+        if(x>y){
+            return {y,x};
+        }
+        return {x,y};
+    }
+};
