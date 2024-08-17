846bb8d7d77ee402cc864949cc15ffaf90707230
Nitesh Yadav <42939377+NiteshShivam@users.noreply.github.com>
2024-08-17 10:03:11 +0530

    Update k largest elements.py

--- Heap/k largest elements.py
+++ Heap/k largest elements.py
@@ -14,3 +14,30 @@ class Solution:
 		        heapq.heappop(hp)
 	    hp.sort(reverse=True)
 	    return hp
+
+
+# cpp
+============================
+
+class Solution{
+public:	
+	vector<int> kLargest(int arr[], int n, int k) {
+	   priority_queue<int,vector<int>,greater<int>> minHeap;
+	   for(int i=0;i<n;i++){
+	       minHeap.push(arr[i]);
+	       if(minHeap.size()>k){
+	           minHeap.pop();
+	       }
+	   }
+	   vector<int>result;
+	   int length = minHeap.size();
+	   while(length--){
+	       result.push_back(minHeap.top());
+	       minHeap.pop();
+	   }
+	   //revese sort
+	   sort(result.rbegin(),result.rend());
+	   return result;
+	}
+
+};
