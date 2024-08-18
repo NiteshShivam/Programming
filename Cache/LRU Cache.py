6ab95874c81462eea8132430b5dece143edbef04
Nitesh Yadav <42939377+NiteshShivam@users.noreply.github.com>
2024-08-18 11:21:46 +0530

    Update LRU Cache.py

--- Cache/LRU Cache.py
+++ Cache/LRU Cache.py
@@ -50,3 +50,41 @@ class LRUCache
     }
 };
 
+
+# python
+======================
+
+class LRUCache:
+     
+    
+    def __init__(self,cap):
+        self.cache = []
+        self.cap = cap
+        
+        
+        
+    #Function to return value corresponding to the key.
+    def get(self, key):
+        for i in range(len(self.cache)):
+            if self.cache[i][0]==key:
+                val = self.cache[i][1]
+                temp = self.cache[i]
+                self.cache.pop(i)
+                self.cache.append(temp)
+                return val
+        return -1
+        
+    #Function for storing key-value pair.   
+    def set(self, key, value):
+        for i in range(len(self.cache)):
+            if self.cache[i][0]==key:
+                self.cache.pop(i)
+                self.cache.append((key,value))
+                return
+        if len(self.cache)<self.cap:
+            self.cache.append((key,value))
+        else:
+            self.cache.pop(0)
+            self.cache.append((key,value))
+
+
