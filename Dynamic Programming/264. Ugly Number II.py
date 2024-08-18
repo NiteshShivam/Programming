baaf78dc83413784e58fdafd4bf7f9ff1cb23f57
Nitesh Yadav <42939377+NiteshShivam@users.noreply.github.com>
2024-08-18 08:31:48 +0530

    Update 264. Ugly Number II.py

--- Dynamic Programming/264. Ugly Number II.py
+++ Dynamic Programming/264. Ugly Number II.py
@@ -1,5 +1,6 @@
 '''
 https://leetcode.com/problems/ugly-number-ii/description/
+https://youtu.be/jRacRh6x4go
 '''
 class Solution:
     def nthUglyNumber(self, n: int) -> int:
@@ -21,3 +22,33 @@ class Solution:
                 five+=1
 
         return ugly[-1]
+
+
+# cpp
+==================================
+class Solution {
+public:
+    int nthUglyNumber(int n) {
+        vector<int> dp(n+1,0);
+        dp[0]=1;
+        int two=0,three=0,five=0;
+        for(int i=1;i<=n;i++){
+            int first = dp[two]*2;
+            int second = dp[three]*3;
+            int third = dp[five]*5;
+            int value = min({first,second,third});
+            //cout<<" "<<value<<endl;
+            if(value==first){
+                two++;
+            }
+            if(value==second){
+                three++;
+            }
+            if(value==third){
+                five++;
+            }
+            dp[i]=value;
+        }
+        return dp[n-1];
+    }
+};
