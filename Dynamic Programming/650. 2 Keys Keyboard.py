49859e154593e2036e566cdd18e0b3ca0b07ea61
Nitesh Yadav <42939377+NiteshShivam@users.noreply.github.com>
2024-08-19 15:57:06 +0530

    Update 650. 2 Keys Keyboard.py

--- Dynamic Programming/650. 2 Keys Keyboard.py
+++ Dynamic Programming/650. 2 Keys Keyboard.py
@@ -2,7 +2,31 @@
 https://leetcode.com/problems/2-keys-keyboard/description/
 https://youtu.be/zELBK8_vB7Y
 '''
+class Solution:
+    def solve(self,n,count,clip):
+        if count==n:
+            return 0
+        if count>n:
+            return float('inf')
+        if self.dp[count][clip]!=-1:
+            return self.dp[count][clip]
+        
+        paste = 1 + self.solve(n,count+clip,clip)
+        copy_paste = 2 + self.solve(n,count+count,count)
+        self.dp[count][clip] =  min(copy_paste,paste)
+        return self.dp[count][clip]
+    def minSteps(self, n: int) -> int:
+        self.dp = [[-1]*(n+1) for _ in range(n+1)]
+        result = 1
+        current = 1
+        if n==1:
+            return 0
+        return  1+self.solve(n,result,current)
 
+
+
+
+===========================
 class Solution:
     def solve(self,n,count,clip):
         if count==n:
