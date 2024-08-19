fadb72362b666889bbe9240e558a8a86c7f360ad
Nitesh Yadav <42939377+NiteshShivam@users.noreply.github.com>
2024-08-19 06:15:59 +0530

    Update Kth Smallest.py

--- array/Kth Smallest.py
+++ array/Kth Smallest.py
@@ -30,3 +30,35 @@ class Solution {
         return maxHeap.top();
     }
 };
+
+
+# using partion quick sort
+===============
+
+
+class Solution:
+    def partition(self,arr,l,r,pivot):
+        arr[l+pivot],arr[r] = arr[r],arr[l+pivot]
+        i = l
+        j = l-1
+        while i<r:
+            if arr[i]<arr[r]:
+                j+=1
+                arr[i],arr[j]=arr[j],arr[i]
+            i+=1
+        arr[j+1],arr[r] = arr[r],arr[j+1]
+        return j+1
+    def kthSmallest(self, arr,k):
+        l =0
+        r=len(arr)-1
+        
+        while l<=r:
+            pivot = (r-l)//2
+            result = self.partition(arr,l,r,pivot)
+            if result==k-1:
+                return arr[result]
+            elif result>k-1:
+                r=result-1
+            else:
+                l=result+1
+        return -1
