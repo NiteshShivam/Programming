77a726bb7e3e606bebbdd67b6b95e14cb261cfa0
Nitesh Yadav <42939377+NiteshShivam@users.noreply.github.com>
2024-08-21 12:07:13 +0530

    Update Shortest path in Undirected Graph.py

--- Graph/Shortest path in Undirected Graph.py
+++ Graph/Shortest path in Undirected Graph.py
@@ -28,3 +28,42 @@ class Solution:
                 result[i] =-1
         return result
                 
+
+
+# cpp
+=========================================================
+
+class Solution {
+  public:
+    vector<int> shortestPath(vector<vector<int>>& edges, int N,int M, int src){
+        int n = N;
+        vector<vector<int>> adj(n);
+        for(const auto &each:edges){
+            adj[each[0]].push_back(each[1]);
+            adj[each[1]].push_back(each[0]);
+        }
+        vector<int> result(n,INT_MAX);
+        result[src]=0;
+        queue<int> q;
+        q.push(src);
+        while(!q.empty()){
+            int length = q.size();
+            while(length--){
+                int vertex = q.front();
+                q.pop();
+                for(int ne : adj[vertex]){
+                    if(result[vertex]+1<result[ne]){
+                        result[ne] = result[vertex]+1;
+                        q.push(ne);
+                    }
+                }
+            }
+        }
+        for(int i=0;i<n;i++){
+            if(result[i]==INT_MAX){
+                result[i]=-1;
+            }
+        }
+        return result;
+    }
+};
