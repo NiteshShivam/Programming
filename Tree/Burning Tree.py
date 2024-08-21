7663dbe8bc296d409af010ae1d64550f4b1b1e78
Nitesh Yadav <42939377+NiteshShivam@users.noreply.github.com>
2024-08-21 14:21:02 +0530

    Update Burning Tree.py

--- Tree/Burning Tree.py
+++ Tree/Burning Tree.py
@@ -2,6 +2,8 @@
 https://www.geeksforgeeks.org/problems/burning-tree/1
 
 '''
+# python
+
 from collections import deque
 class Solution:
 
@@ -45,4 +47,61 @@ class Solution:
         if result>=1:
             return result-1
         return result
-        
+
+# cpp
+=====================================
+
+class Solution {
+  public:
+    int minTime(Node* root, int target) 
+    {
+        unordered_map<Node*,Node*> parent;
+        parent[root]=nullptr;
+        queue<Node*> q;
+        q.push(root);
+        Node* node;
+        Node* Target;
+        while(!q.empty()){
+            node = q.front();
+            if(node->data==target){
+                Target=node;
+            }
+            q.pop();
+            if(node->left){
+                parent[node->left]=node;
+                q.push(node->left);
+            }
+            if(node->right){
+                parent[node->right]=node;
+                q.push(node->right);
+            }
+            
+        }
+        int result=0;
+        unordered_set<Node*> visited;
+        visited.insert(Target);
+        q.push(Target);
+        while(!q.empty())
+        {
+            int length = q.size();
+            while(length--){
+             node = q.front();
+             q.pop();
+             if(node->left && visited.find(node->left)==visited.end()){
+                 visited.insert(node->left);
+                 q.push(node->left);
+             }
+             if(node->right && visited.find(node->right)==visited.end()){
+                 visited.insert(node->right);
+                 q.push(node->right);
+             }
+             if(parent[node] && visited.find(parent[node])==visited.end()){
+                 visited.insert(parent[node]);
+                 q.push(parent[node]);
+             }
+            }
+            result++;
+            
+        }
+        return result>0 ?result-1:-1;
+    }
