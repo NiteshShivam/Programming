921e79890f2223fe5f9ee768cc922b73c9d6c1de
Nitesh Yadav <42939377+NiteshShivam@users.noreply.github.com>
2024-08-22 08:39:24 +0530

    Update Chocolate Distribution Problem.py

--- array/Chocolate Distribution Problem.py
+++ array/Chocolate Distribution Problem.py
@@ -1,5 +1,8 @@
 '''
 https://www.geeksforgeeks.org/problems/chocolate-distribution-problem3825/1
+
+solution:
+https://discuss.geeksforgeeks.org/comment/2cf8486841cf4b75932562b71295c2e2/practice
 '''
 class Solution:
 
