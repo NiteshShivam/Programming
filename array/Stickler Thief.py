fd7fec41a88d4e7b401f8c97a9f8c4f4729bb264
Nitesh Yadav <42939377+NiteshShivam@users.noreply.github.com>
2024-08-22 10:49:58 +0530

    Update Stickler Thief.py

--- array/Stickler Thief.py
+++ array/Stickler Thief.py
@@ -1,6 +1,37 @@
 '''
 https://www.geeksforgeeks.org/problems/stickler-theif-1587115621/1
+
+https://youtu.be/SI6Pm8AKqnQ
 '''
+
+# cpp
+class Solution
+{
+    public:
+    vector<int> dp;
+    int solve(int arr[],int index,int n){
+        if(index>=n){
+            return 0;
+        }
+        if(dp[index]!=-1){
+            return dp[index];
+        }
+        int l = arr[index]+solve(arr,index+2,n);
+        int r= solve(arr,index+1,n);
+        return dp[index] = max(l,r);
+    }
+    int FindMaxSum(int arr[], int n)
+    {
+        dp.resize(n,-1);
+        int index = 0;
+        return solve(arr,index,n);
+    }
+    
+};
+
+
+
+=========================
 class Solution:  
     
     #Function to find the maximum money the thief can get.
