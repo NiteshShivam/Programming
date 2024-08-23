d66297a5543dc4559d787c0d56c8c9a63d851801
Nitesh Yadav <42939377+NiteshShivam@users.noreply.github.com>
2024-08-23 11:59:14 +0530

    Update Longest Palindrome in a String.py

--- String/Longest Palindrome in a String.py
+++ String/Longest Palindrome in a String.py
@@ -3,6 +3,45 @@ https://www.geeksforgeeks.org/problems/longest-palindrome-in-a-string3411/1
 '''
 class Solution:
     def longestPalin(self, S):
+        # code here
+        length = len(S)
+        result = 0
+        start = 0
+        end = 0
+        for i in range(length):
+            l = i
+            r = i
+            while l>=0 and r<length:
+                if S[l]!=S[r]:
+                    break
+                l-=1
+                r+=1
+            le = r-l-1
+            if result<le:
+                result = le
+                start = l+1
+                end = r-1
+        
+        for i in range(length):
+            l = i
+            r = i+1
+            while l>=0 and r<length:
+                if S[l]!=S[r]:
+                    break
+                l-=1
+                r+=1
+            le = r-l-1
+            if result<le:
+                result = le
+                start = l+1
+                end = r-1
+        return S[start:start+result]
+        
+
+
+==========================
+class Solution:
+    def longestPalin(self, S):
         length = len(S)
         dp = [[False]*length for i in range(length)]
         for i in range(length):
