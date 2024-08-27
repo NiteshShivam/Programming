612f73c06b20b0978cb45bf2e97429847f4a3ff4
Nitesh Yadav <42939377+NiteshShivam@users.noreply.github.com>
2024-08-27 09:11:39 +0530

    Update 1514. Path with Maximum Probability.py

--- Graph/1514. Path with Maximum Probability.py
+++ Graph/1514. Path with Maximum Probability.py
@@ -26,3 +26,42 @@ class Solution:
         if result[end_node]==float('-inf'):
             return  0.00000
         return result[end_node]
+
+
+# cpp
+======================================
+class Solution {
+public:
+    typedef pair<double,int>p;
+    double maxProbability(int n, vector<vector<int>>& edges, vector<double>& succProb, int start_node, int end_node) {
+        unordered_map<int,vector<pair<int,double>>> adj;
+        vector<double> result(n,0);
+        for(int i=0;i<edges.size();i++)
+        {
+            adj[edges[i][0]].push_back({edges[i][1],succProb[i]});
+            adj[edges[i][1]].push_back({edges[i][0],succProb[i]});
+        }
+        priority_queue<p> pq;
+        
+        result[start_node]=1.0;
+        pq.push({1.0,start_node});
+        while(!pq.empty()){
+            int currentNode = pq.top().second;
+            double prob = pq.top().first;
+            pq.pop();
+            for(auto &temp:adj[currentNode]){
+                double adjProb = temp.second;
+                int adjNode = temp.first;
+                if(result[adjNode]<adjProb*prob){
+                    result[adjNode]=adjProb*prob;
+                    pq.push({result[adjNode],adjNode});
+                }
+            }
+
+        }
+        if(result[end_node]==0){
+            return 0.0;
+        }
+        return result[end_node];
+    }
+};
