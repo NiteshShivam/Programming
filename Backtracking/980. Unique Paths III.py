f64e7fe581941a986713aa0367e113ee87f5f39e
Nitesh Yadav <42939377+NiteshShivam@users.noreply.github.com>
2024-08-28 19:31:07 +0530

    Update 980. Unique Paths III.py

--- Backtracking/980. Unique Paths III.py
+++ Backtracking/980. Unique Paths III.py
@@ -40,4 +40,52 @@ class Solution:
         self.solve(grid,startI,startJ,0)
         
         return self.result
-        
+
+
+# cpp
+===============================================
+class Solution {
+public:
+    int m,n,result,nonObj;
+    vector<vector<int>> direction{{1,0},{-1,0},{0,1},{0,-1}};
+    void backTrack(vector<vector<int>>& grid,int i,int j,int count){
+        if(i<0 || i>=m || j<0 || j>=n || grid[i][j]==-1){
+            return;
+        }
+        if(grid[i][j]==2){
+            if(count==nonObj){
+                result++;
+            }
+            return;
+        }
+        grid[i][j]=-1;
+        for(vector<int>&dir:direction){
+            int new_i = i+dir[0];
+            int new_j = j+dir[1];
+            backTrack(grid,new_i,new_j,count+1);
+        }
+        grid[i][j]=0;
+    }
+    int uniquePathsIII(vector<vector<int>>& grid) {
+        m = grid.size();
+        n = grid[0].size();
+        nonObj=0;
+        result=0;
+        int start_x=0;
+        int start_y =0;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j]==0 || grid[i][j]==1){
+                    nonObj+=1;
+                }
+                if(grid[i][j]==1){
+                    start_x=i;
+                    start_y=j;
+                }
+            }
+        }
+        int count=0;
+        backTrack(grid,start_x,start_y,count);
+        return result;
+    }
+};
