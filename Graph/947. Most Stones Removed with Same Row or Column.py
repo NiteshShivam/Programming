6c7b6395571a25c186a5c6a678dfacaa08efe57a
Nitesh Yadav <42939377+NiteshShivam@users.noreply.github.com>
2024-08-29 15:03:35 +0530

    Update 947. Most Stones Removed with Same Row or Column.py

--- Graph/947. Most Stones Removed with Same Row or Column.py
+++ Graph/947. Most Stones Removed with Same Row or Column.py
@@ -21,3 +21,34 @@ class Solution:
                 self.dfs(stones,i)
                 group+=1
         return self.n -group
+
+
+
+# cpp
+=========================
+
+class Solution {
+public:
+    void dfs(vector<bool>& visited,vector<vector<int>>& stones,int index){
+        visited[index]=true;
+        for(int i=0;i<stones.size();i++){
+            if((visited[i]==false)&&
+            
+            (stones[index][0]==stones[i][0] || stones[index][1]==stones[i][1])){
+                dfs(visited,stones,i);
+            }
+        }
+    }
+    int removeStones(vector<vector<int>>& stones) {
+        int n = stones.size();
+        vector<bool> visited(n,false);
+        int group=0;
+        for(int i=0;i<n;i++){
+            if(visited[i]==false){
+                dfs(visited,stones,i);
+                group+=1;
+            }
+        }
+        return n-group;
+    }
+};
