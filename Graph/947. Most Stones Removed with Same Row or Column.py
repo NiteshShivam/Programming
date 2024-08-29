436159aeee33135b818fc566f2770b2f348d1656
Nitesh Yadav <42939377+NiteshShivam@users.noreply.github.com>
2024-08-29 16:25:31 +0530

    Update 947. Most Stones Removed with Same Row or Column.py

--- Graph/947. Most Stones Removed with Same Row or Column.py
+++ Graph/947. Most Stones Removed with Same Row or Column.py
@@ -3,7 +3,11 @@ https://leetcode.com/problems/most-stones-removed-with-same-row-or-column/descri
 
 mik-video
 
+dfs
 https://youtu.be/ZsGTpXm966E
+
+dsu
+https://youtu.be/cmqIeBFx01w
 
 '''
 class Solution:
@@ -52,3 +56,42 @@ public:
         return n-group;
     }
 };
+
+
+
+
+# python dsu
+======================================
+
+class Solution:
+    def find(self,i):
+        if i==self.parent[i]:
+            return i
+        self.parent[i] =self.find(self.parent[i])
+        return self.parent[i]
+    def union(self,x,y):
+        parentx = self.find(x)
+        parenty = self.find(y)
+        if parentx==parenty:
+            return
+        if self.rank[parentx]>self.rank[parenty]:
+            self.parent[parenty]=parentx
+        elif self.rank[parentx]<self.rank[parenty]:
+            self.parent[parentx]=parenty
+        else:
+            self.rank[parenty] = self.rank[parenty]+1
+            self.parent[parentx]=parenty
+    def removeStones(self, stones: List[List[int]]) -> int:
+        self.length = len(stones)
+        self.rank = [1]*self.length
+        self.parent = [i for i in range(self.length)]
+
+        for i in range(self.length):
+            for j in range(i+1,self.length):
+                if stones[i][0]==stones[j][0] or stones[i][1]==stones[j][1]:
+                    self.union(i,j)
+        group = 0
+        for i in range(self.length):
+            if self.parent[i]==i:
+                group+=1
+        return self.length-group
