cbc8830662e60beed60ab01dac3eaf300014d9ca
Nitesh Yadav <42939377+NiteshShivam@users.noreply.github.com>
2024-09-01 08:01:10 +0530

    Update Shortest Common Supersequence.cpp

--- String/Shortest Common Supersequence.cpp
+++ String/Shortest Common Supersequence.cpp
@@ -89,3 +89,31 @@ class Solution
         return t[m][n];
     }
 };
+
+
+// using LCS
+=============================================
+ 
+class Solution
+{
+    public:
+    //Function to find length of shortest common supersequence of two strings.
+    int shortestCommonSupersequence(string s1, string s2, int m, int n)
+    {
+        vector<vector<int>>t(m+1,vector<int>(n+1));
+        for(int i=0;i<m+1;i++){
+            for(int j=0;j<n+1;j++){
+                if(i==0 || j==0){
+                    t[i][j]=0;
+                }
+                else if(s1[i-1]==s2[j-1]){
+                    t[i][j]=1+t[i-1][j-1];
+                }
+                else{
+                    t[i][j]=max(t[i-1][j],t[i][j-1]);
+                }
+            }
+        }
+        return m+n- t[m][n];
+    }
+};
