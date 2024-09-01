2fab2530de8a537f84240604d6fc7d339f9473d8
Nitesh Yadav <42939377+NiteshShivam@users.noreply.github.com>
2024-09-01 14:28:09 +0530

    Update 5. Longest Palindromic Substring.cpp

--- String/5. Longest Palindromic Substring.cpp
+++ String/5. Longest Palindromic Substring.cpp
@@ -4,6 +4,51 @@ mik-video:
 https://youtu.be/ij3X5SAhf_0?list=PLpIkg8OmuX-JhFpkhgrAwZRtukO0SkwAt
   */
 
+
+// recursion with memo.
+class Solution {
+public:
+    vector<vector<int>>t;
+    bool palindrome(string& s,int i,int j){
+
+        if(i>=j){
+            return true;
+        }
+        if(t[i][j]!=-1){
+            return t[i][j];
+        }
+        if(s[i]==s[j]){
+            return t[i][j]= palindrome(s,i+1,j-1);
+        }
+        else{
+            return t[i][j] =false;
+        }
+    }
+    string longestPalindrome(string s) {
+        int n = s.length();
+        t = vector<vector<int>> (n,vector<int>(n,-1));
+        // t= (n,vector<int>(n,-1));
+        int maxlen =0;
+        int start=0;
+        for(int i=0;i<n;i++){
+            for(int j=i;j<n;j++){
+                if(palindrome(s,i,j)){
+                    if(j-i+1>maxlen){
+                        maxlen=j-i+1;
+                        start=i;
+                    }
+                }
+            }
+        }
+        return s.substr(start,maxlen);
+    }
+};
+
+
+
+// bottom -up
+=========================================================
+ 
 class Solution {
 public:
     string longestPalindrome(string s) {
