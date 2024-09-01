5985bd936587854f607f3941bf1c141d895c2686
Nitesh Yadav <42939377+NiteshShivam@users.noreply.github.com>
2024-09-01 17:21:35 +0530

    Update 2022. Convert 1D Array Into 2D Array.cpp

--- array/2022. Convert 1D Array Into 2D Array.cpp
+++ array/2022. Convert 1D Array Into 2D Array.cpp
@@ -1,5 +1,8 @@
 /*
 https://leetcode.com/problems/convert-1d-array-into-2d-array/description/
+
+mik-video
+https://youtu.be/4jQErv8WK6o
   */
 class Solution {
 public:
@@ -21,6 +24,32 @@ public:
                 temp.clear();
             }
 
+
+        }
+        return result;
+    }
+};
+
+
+
+
+=================================================
+
+ class Solution {
+public:
+    vector<vector<int>> construct2DArray(vector<int>& original, int m, int n) {
+        int total = m*n;
+        if(total!=original.size()){
+            return {};
+        }
+        int i;
+        int j;
+        vector<vector<int>>result(m,vector<int>(n));
+        
+        for(int k=0;k<original.size();k++){
+            i=k/n;
+            j=k%n;
+            result[i][j]=original[k];
 
         }
         return result;
