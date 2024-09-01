4c0d08221ec875fa9f6591a1f640dfbd7dab922f
Nitesh Yadav <42939377+NiteshShivam@users.noreply.github.com>
2024-09-01 07:49:11 +0530

    Update Shortest Common Supersequence.cpp

--- String/Shortest Common Supersequence.cpp
+++ String/Shortest Common Supersequence.cpp
@@ -61,3 +61,31 @@ class Solution
         return solve(X,Y,m,n);
     }
 };
+
+
+// bottom up approach O(m*n)
+==================================================
+
+ class Solution
+{
+    public:
+    //Function to find length of shortest common supersequence of two strings.
+    int shortestCommonSupersequence(string s1, string s2, int m, int n)
+    {
+        vector<vector<int>>t(m+1,vector<int>(n+1));
+        for(int i=0;i<m+1;i++){
+            for(int j=0;j<n+1;j++){
+                if(i==0 || j==0){
+                    t[i][j]=i+j;
+                }
+                else if(s1[i-1]==s2[j-1]){
+                    t[i][j]=1+t[i-1][j-1];
+                }
+                else{
+                    t[i][j]=1+min(t[i-1][j],t[i][j-1]);
+                }
+            }
+        }
+        return t[m][n];
+    }
+};
