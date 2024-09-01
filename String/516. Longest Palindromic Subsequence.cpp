7217997e1ce6a609e60da27f475d824292b9d269
Nitesh Yadav <42939377+NiteshShivam@users.noreply.github.com>
2024-09-01 15:51:12 +0530

    Update 516. Longest Palindromic Subsequence.cpp

--- String/516. Longest Palindromic Subsequence.cpp
+++ String/516. Longest Palindromic Subsequence.cpp
@@ -27,3 +27,65 @@ public:
         return solve(s,0,s.size()-1);
     }
 };
+
+
+
+// LCS recursion + memo.
+======================================
+
+ class Solution {
+public:
+    int t[1001][1001];
+    int LCS(string& s1,string &s2,int m,int n){
+        if(m==0 || n==0){
+            return t[m][n]=0;
+        }
+        if(t[m][n]!=-1)
+            return t[m][n];
+        if(s1[m-1]==s2[n-1]){
+            return t[m][n] = 1+LCS(s1,s2,m-1,n-1);
+        }
+        return t[m][n] = max(LCS(s1,s2,m-1,n),LCS(s1,s2,m,n-1));
+    }
+    int longestPalindromeSubseq(string s) {
+        memset(t,-1,sizeof(t));
+        int m = s.size();
+        string s2 = s;
+        reverse(s2.begin(),s2.end());
+        return LCS(s,s2,m,m);
+        
+    }   
+};
+
+
+
+// LCS bottom up
+===============================
+ class Solution {
+public:
+    int t[1001][1001];
+    int LCS(string& s1,string &s2,int m,int n){
+        for(int i=0;i<=m;i++){
+            for(int j=0;j<=n;j++){
+                if(i==0||j==0){
+                    t[i][j]=0;
+                }
+                else if(s1[i-1]==s2[j-1]){
+                    t[i][j] = 1+ t[i-1][j-1];
+                }
+                else{
+                    t[i][j] = max(t[i-1][j],t[i][j-1]);
+                }
+            }
+        }
+        return t[m][n];
+    }
+    int longestPalindromeSubseq(string s) {
+        memset(t,-1,sizeof(t));
+        int m = s.size();
+        string s2 = s;
+        reverse(s2.begin(),s2.end());
+        return LCS(s,s2,m,m);
+        
+    }   
+};
