546b3d81e0185447c913508c2c3aa035fcddf88c
Nitesh Yadav <42939377+NiteshShivam@users.noreply.github.com>
2024-09-03 12:00:07 +0530

    Update Dijkstra Algorithm.py

--- Graph/Dijkstra Algorithm.py
+++ Graph/Dijkstra Algorithm.py
@@ -12,6 +12,10 @@ Note: The Graph doesn't contain any negative weight cycle.
 https://www.geeksforgeeks.org/problems/implementing-dijkstra-set-1-adjacency-matrix/1
 
 https://youtu.be/xQ3vjWwFRuI
+
+
+using-set mik
+https://youtu.be/3qIoYIMidpc?list=PLpIkg8OmuX-LZB9jYzbbZchk277H5CbdY
 '''
 import sys
 import heapq
@@ -33,3 +37,42 @@ class Solution:
                     result[adjNode]=distance+weight
                     heapq.heappush(pq,(distance+weight,adjNode))
         return result
+
+
+
+============================
+
+
+class Solution
+{
+	public:
+	//Function to find the shortest distance of all the vertices
+    //from the source vertex S.
+    vector <int> dijkstra(int V, vector<vector<int>> adj[], int S)
+    {
+       vector<int> result(V,INT_MAX);
+       result[S]=0;
+       set<pair<int,int>> s;
+       s.insert({0,S});
+       while(!s.empty()){
+           auto &it = *s.begin();
+           int d= it.first;
+           int node =it.second;
+           s.erase(it);
+           for(auto &vec:adj[node]){
+               int adjNode =vec[0];
+               int dist = vec[1];
+               if(d+dist<result[adjNode]){
+                   if(result[adjNode]!=INT_MAX){
+                       s.erase({result[adjNode],adjNode});
+                   }
+                   result[adjNode]=d+dist;
+                   
+                   s.insert({d+dist,adjNode});
+               }
+           }
+           
+       }
+       return result;
+    }
+};
