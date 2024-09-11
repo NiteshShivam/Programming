ca5af7b9f9270606ddfd7d07d8bfe4f298d9242e
Nitesh Yadav <42939377+NiteshShivam@users.noreply.github.com>
2024-09-11 10:48:38 +0530

    Create 3217. Delete Nodes From Linked List Present in Array.cpp

---
+++ Linked List/3217. Delete Nodes From Linked List Present in Array.cpp
@@ -0,0 +1,40 @@
+/*
+https://leetcode.com/problems/delete-nodes-from-linked-list-present-in-array/description/
+mik-video:
+https://youtu.be/qb7br7auuOc
+*/
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* modifiedList(vector<int>& nums, ListNode* head) {
+        unordered_set<int> st(begin(nums),end(nums));
+        while(head!=NULL && st.find(head->val)!=st.end()){
+            ListNode *temp = head;
+            head = head->next;
+            delete(temp);
+        }
+        ListNode *curr=head;
+        while(curr!=NULL && curr->next!=NULL){
+            if(st.find(curr->next->val)!=st.end()){
+                ListNode *temp=curr->next;
+                
+                curr->next=curr->next->next;
+                delete(temp);
+            }
+            else
+            curr=curr->next;
+        }
+        
+        return head;
+
+    }
+};
