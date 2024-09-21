3f94164cfc8c779a16658caf5c1c400f295ce104
Nitesh Yadav <42939377+NiteshShivam@users.noreply.github.com>
2024-09-21 14:14:39 +0530

    Update 386. Lexicographical Numbers.cpp

--- Graph/386. Lexicographical Numbers.cpp
+++ Graph/386. Lexicographical Numbers.cpp
@@ -1,5 +1,7 @@
 /*
 https://leetcode.com/problems/lexicographical-numbers/description/
+mik-video:
+https://youtu.be/vuS3US_bDBo
   */
 class Solution {
 public:
@@ -16,5 +18,32 @@ public:
             return s1<s2;
         });
         return result;
+    }
+};
+
+
+// using dfs
+=====================================================
+ class Solution {
+public:
+    void dfs(int i,vector<int>& result,int n){
+        if(i>n){
+            return;
+        }
+        for(int j=0;j<=9;j++){
+            if(i*10+j<=n){
+                result.push_back(i*10+j);
+                dfs(i*10+j,result,n);
+            }
+        }
+    }
+    vector<int> lexicalOrder(int n) {
+     vector<int> result;
+     for(int i=1;i<=9;i++){
+        if(i<=n){
+        result.push_back(i);
+        dfs(i,result,n);}
+     }
+     return result;   
     }
 };
