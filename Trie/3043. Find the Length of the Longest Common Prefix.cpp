5662e319236cea9650c8d03e22075ec0719104da
Nitesh Yadav <42939377+NiteshShivam@users.noreply.github.com>
2024-09-24 11:00:25 +0530

    Update 3043. Find the Length of the Longest Common Prefix.cpp

--- Trie/3043. Find the Length of the Longest Common Prefix.cpp
+++ Trie/3043. Find the Length of the Longest Common Prefix.cpp
@@ -4,6 +4,58 @@ mik-video:
 
 https://youtu.be/OZNS72LANFU
   */
+struct TrieNode{
+    TrieNode* children[10];
+};
+class Solution {
+public:
+    TrieNode* getTrieNode(){
+        TrieNode* node = new TrieNode();
+        for(int i=0;i<10;i++){
+            node->children[i]=nullptr;
+        }
+        return node;
+    }
+    void insert(int num,TrieNode* root){
+        TrieNode* crawl =root;
+        string n = to_string(num);
+        for(char ch:n){
+            int idx = ch-'0';
+            if(crawl->children[idx]==nullptr){
+                crawl->children[idx]=getTrieNode();
+            }
+            crawl=crawl->children[idx];
+        }
+    }
+    int  search(int num,TrieNode* root){
+        TrieNode* crawl = root;
+        string n = to_string(num);
+        int length=0;
+        for(char ch:n){
+            int idx = ch-'0';
+            if(crawl->children[idx]!=nullptr){
+                length+=1;
+                crawl = crawl->children[idx];
+            }
+            else{
+                break;
+            }
+        }
+        return length;
+    }
+
+    int longestCommonPrefix(vector<int>& arr1, vector<int>& arr2) {
+        TrieNode* root = getTrieNode();
+        for(int num:arr1){
+            insert(num,root);
+        }
+        int result = 0;
+        for(int num : arr2){
+            result = max(result,search(num,root));
+        }
+        return result;
+    }
+};
 
 
 ===========================================================
