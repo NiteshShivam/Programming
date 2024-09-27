f4be08ffe06f622199e181ad980357582b8f2c5d
Nitesh Yadav <42939377+NiteshShivam@users.noreply.github.com>
2024-09-27 21:50:48 +0530

    Update 729. My Calendar I.py

--- array/729. My Calendar I.py
+++ array/729. My Calendar I.py
@@ -16,6 +16,35 @@ class MyCalendar:
         self.s.append([start,end])
         return True
 
+
+
+=============================
+
+class MyCalendar {
+public:
+    set<pair<int,int>> st;
+    MyCalendar() {
+        
+        
+    }
+    
+    bool book(int start, int end) {
+        auto it = st.lower_bound({start,end});
+    
+    if(it!=st.end() && it->first<end){
+        return false;
+    }
+    if(it!=st.begin()){
+        auto prevI = prev(it);
+        if(start<prevI->second){
+            return false;
+        }
+    } 
+    st.insert({start,end});
+    return true;
+    }
+};
+
 # Your MyCalendar object will be instantiated and called as such:
 # obj = MyCalendar()
 # param_1 = obj.book(start,end)
