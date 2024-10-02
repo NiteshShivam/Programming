ed13934dfb4f95f094c8109a6e76ba7c46e39fb6
Nitesh Yadav <42939377+NiteshShivam@users.noreply.github.com>
2024-10-02 05:52:32 +0530

    Update 1331. Rank Transform of an Array.py

--- Hash/1331. Rank Transform of an Array.py
+++ Hash/1331. Rank Transform of an Array.py
@@ -14,3 +14,24 @@ class Solution:
         for i in arr:
             result.append(mp[i])
         return result
+
+# c++
+====================================
+class Solution {
+public:
+    vector<int> arrayRankTransform(vector<int>& arr) {
+        vector<int> A (arr);
+        sort(A.begin(),A.end());
+        unordered_map<int,int> st;
+        for(int i=0;i<A.size();i++){
+            if(st.find(A[i])==st.end()){
+                st[A[i]]=st.size()+1;
+            }
+        }
+        vector<int>result;
+        for(const auto &num:arr){
+            result.push_back(st[num]);
+        }
+        return result;
+    }
+};
