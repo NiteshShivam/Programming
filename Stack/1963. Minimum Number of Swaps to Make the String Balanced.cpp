99f78f165fc8bc11c6142acf5ae1165b23c33366
Nitesh Yadav <42939377+NiteshShivam@users.noreply.github.com>
2024-10-08 14:18:30 +0530

    Create 1963. Minimum Number of Swaps to Make the String Balanced.cpp

---
+++ Stack/1963. Minimum Number of Swaps to Make the String Balanced.cpp
@@ -0,0 +1,33 @@
+/*
+https://leetcode.com/problems/minimum-number-of-swaps-to-make-the-string-balanced/description/
+mik-video
+https://youtu.be/W61jIP-O8lw
+  */
+class Solution {
+public:
+    int minSwaps(string s) {
+        stack<char> st;
+        for(int i=0;i<s.size();i++){
+            if(st.empty()){
+                st.push(s[i]);
+            }
+            else{
+                if(s[i]==']'){
+                    if(st.top()=='['){
+                        st.pop();
+                    }
+                    else{
+                        st.push(s[i]);
+                    }
+                }
+                else {
+                        st.push(s[i]);
+                    }
+              
+            }
+        
+    }
+    int r = st.size()/2;
+    return (r+1)/2;
+    }
+};
